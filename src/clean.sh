0780a9b195362ea8f566bcf4217ab92d48ae507a
kolacz <kolaczjl@o2.pl>
2019-06-27 23:22:51 +0200

    Databases project init

---
+++ src/clean.sh
@@ -0,0 +1,3 @@
+
+psql -U postgres -d student -c "DROP SCHEMA public CASCADE"
+psql -U postgres -d
---
+++ src/database.scala
@@ -0,0 +1,5 @@
+object database {
+  def main(args: Array[String]): Unit = {
+    println("fasfsa")
+  }
+}
